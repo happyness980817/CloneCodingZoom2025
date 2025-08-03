0cab6c85ab63c3f71cf0995adde9d47fbbeb7140
Paul Jang <happyness980817@gmail.com>
2025-08-03 20:06:13 +0900

    commonJS -> ESM

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 import http from "http";
-import WebSocket from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+const wss = new WebSocketServer({ server });
 
 server.listen(3000, handleListen);
